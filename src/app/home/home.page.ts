4e9fb0e5aa6dc16ad2bfab62ebfba70ab033beea
WALLACE076300 <pedrosowallace630@gmail.com>
2025-07-30 16:27:59 -0300

    da

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,17 +12,23 @@ import { Router } from '@angular/router';
 })
 export class HomePage {
 
-  pokemon:any = {
-    nome: null,
-    poder: null
-  };
+  clientes:any = [];
 
   constructor( 
     public crudService: CrudService
-  ){ }
+  ){
 
-  enviar() {
-    this.crudService.insert(this.pokemon, 'pokemons');
+    this.getClientes();
+
+   }
+
+  getClientes(){
+    fetch('http://127.0.0.1:8000/api/clientes')
+    .then(resp => resp.json())
+    .then(resp => {
+      console.log(resp);
+      this.clientes = resp;
+    })
   }
 
 }
